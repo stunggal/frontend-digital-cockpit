afb00550f845414f8e5a5e8454d6618a1ba15bcd
stunggal <walfaridsi@gmail.com>
2025-12-06 10:49:09 +0700

    Deployment: Auto-push 2025-12-06 10:49:09

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'my-pwa-cache-v7'; // Ganti versi cache saat ada perubahan aset!
-const OFFLINE_URL = '/offline.html';
+const CACHE_NAME = 'my-pwa-cache-v8'; // Ganti versi cache saat ada perubahan aset!
+const OFFLINE_URL = '/offline';
 
 // Daftar aset-aset penting yang akan di-cache saat Service Worker diinstal
 // Pastikan semua aset yang direferensikan di offline.html ada di sini
